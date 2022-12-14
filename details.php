1c3e11832a9d488df34fd3c5771c3ed0efe53699
dannypijl <1058952@hr.nl>
2022-12-14 14:45:28 +0100

    details

--- details.php
+++ details.php
@@ -36,6 +36,7 @@ mysqli_close($db);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Details - <?= $appointment['firstname'], $appointment['lastname'] ?></title>
 </head>
 <body>
@@ -52,6 +53,26 @@ mysqli_close($db);
         <a class="button" href="dashboard.php">Go back to the list</a>
     </div>
 </div>
+<div class="container-md">
+    <div class="card text-center">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <h1 class="title mt-4"><?= $appointment['firstname'] ?> <?= $appointment['lastname'] ?></h1>
+            <ul>
+                <li>Amount: <?= $appointment['amount'] ?></li>
+                <li>Address: <?= $appointment['address'] ?></li>
+                <li>Date: <?= $appointment['date'] ?></li>
+            </ul>
+            <a href="dashboard.php" class="btn btn-primary">Go back to Dashboard</a>
+        </div>
+        <div class="card-footer text-muted">
+            2 days ago
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
 
