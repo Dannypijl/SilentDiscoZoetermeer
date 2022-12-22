06aa6cc7c22e4ab5bdafaea446640d4984cdc56b
Danny <1058952@hr.nl>
2022-12-22 17:40:21 +0100

    a

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,16 @@
 <?php
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/footer.css"/>
+    <title>Document</title>
+</head>
+<body>
 <footer>
     <section id="footer">
         <div class="footer container">
@@ -24,3 +35,5 @@
     <!-- End Footer -->
     <script src="js/apps.js"></script>
 </footer>
+</body>
+</html>
